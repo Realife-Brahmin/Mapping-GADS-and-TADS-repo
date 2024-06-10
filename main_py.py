8f241f84b29d74abf5c6db793881cc2fa745104c
Realife-Brahmin <aryan.jha@nerc.net>
2024-06-10 11:27:31 -0400

    some  print statements describing df sizes added

--- main_py.py
+++ main_py.py
@@ -4,7 +4,6 @@ import os
 import sys
 import IPython
 
-
 # %%
 try:
     fileAddr = __vsc_ipynb_file__ # type: ignore # vscode only, Pylance seems to have an issue with this macro, so ignoring the warning
@@ -27,16 +26,22 @@ matchedIndices = dfMatch['Rec_ID'].notna()
 num = sum(matchedIndices)
 dfMatched = dfMatch[matchedIndices]
 dfMatched.to_excel("dfMatched.xlsx")
+sizeMatched0 = dfMatched.shape
+print(f"Size of Match_file csv before any filtering: {sizeMatched0[0]}, {sizeMatched0[1]}")
 
 # %%
 tadsFileAddr = os.path.join(rawDataFolder, "TADS 2024 AC Inventory.csv")
 dfTads = pd.read_csv(tadsFileAddr)
+sizeTads0 = dfTads.shape
+print(f"Size of TADS db before filtering: {sizeTads0[0]}, {sizeTads0[1]}")
 # display(dftads)
 
 # %%
 veloFileAddr = os.path.join(rawDataFolder, "tlines-near-chicago-ohare-raw.xlsx") # tlines which are <= 50miles from `Chicago/Ohare` weather station
 print(veloFileAddr)
 dfVelo0 = pd.read_excel(veloFileAddr, engine='openpyxl')
+sizeVelo0 = dfVelo0.shape
+print(f"Size of velocity suite db before any filtering: {sizeVelo0[0]}, {sizeVelo0[1]}")
 # dfVelo0
 
 # %%
@@ -46,14 +51,10 @@ dfVelo = dfVelo0[ dfVelo0['Company Name'] != 'Undetermined Company' ]
 dfVelo = dfVelo[ dfVelo['Voltage kV'] >= 100 ]
 # Filter tlines not currently in service
 dfVelo = dfVelo[ dfVelo['Proposed'] == 'In Service']
-dfVelo
+sizeVelo = dfVelo.shape
+print(f"Size of velocity suite db before any filtering: {sizeVelo[0]}, {sizeVelo[1]}")
+# dfVelo
 
-
-# %% [markdown]
-# 
-
-# %% [markdown]
-# 
 
 # %%
 dfVeloMatched = dfVelo[dfVelo['Rec_ID'].isin(dfMatched['Rec_ID'])]
@@ -61,6 +62,9 @@ dfVeloMatched
 veloMatchedAddr = os.path.join(processedDataFolder, "dfVeloMatched.xlsx")
 veloMatchedAddr
 dfVeloMatched.to_excel("dfVeloMatched.xlsx")
+sizeVelo = dfVelo.shape
+print(f"Size of velocity suite db before any filtering: {sizeVelo[0]}, {sizeVelo[1]}")
+# dfVeloMatched
 
 # %%
 

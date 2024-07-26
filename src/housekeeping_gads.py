494f65370bc23d1a2d00e17e714ff5f94b6f4b8f
Realife-Brahmin <aryan.jha@nerc.net>
2024-07-26 17:04:53 -0400

    filter_states now a more intelligent function. Good work my friend! We now use `us` module to map states to acronyms

--- src/housekeeping_gads.py
+++ src/housekeeping_gads.py
@@ -1,15 +1,16 @@
 # %%
 # pylint: disable=undefined-variable line-too-long invalid-name missing-function-docstring f-string-without-interpolation
 import pandas as pd
+import us
 
-def filter_states(dfGads, states_to_keep=["Illinois", "Indiana", "Wisconsin"]):
-    # Define the list of state names to filter
-    # states_to_keep = ["IL", "IN", "WI"]
+# def filter_states(dfGads, states_to_keep=["Illinois", "Indiana", "Wisconsin"]):
+#     # Define the list of state names to filter
+#     # states_to_keep = ["IL", "IN", "WI"]
 
-    # Filter the DataFrame
-    dfFiltered = dfGads[dfGads["StateName"].isin(states_to_keep)]
+#     # Filter the DataFrame
+#     dfFiltered = dfGads[dfGads["StateName"].isin(states_to_keep)]
 
-    return dfFiltered
+#     return dfFiltered
 
 def filter_by_eia_code(dfVelo, dfGads):
     # Get the unique 'EIA ID' values from dfVelo
@@ -25,5 +26,30 @@ def filter_non_empty_eia_id(dfVeloSorted):
     dfVeloEIA = dfVeloSorted.dropna(subset=["EIA ID"]).copy()
 
     return dfVeloEIA
+
+
+def filter_states(dfGads, veloStates):
+    # Create a mapping of full state names to their abbreviations using the us package
+    state_abbreviations = {state.name: state.abbr for state in us.states.STATES}
+
+    # Print the state abbreviations mapping for debugging
+    print("State Abbreviations Mapping:")
+    print(state_abbreviations)
+
+    # Map StateName to state abbreviations
+    dfGads["StateAbbreviation"] = dfGads["StateName"].map(state_abbreviations)
+
+    # Print a sample of the DataFrame to debug the mapping
+    print("Sample of dfGads with StateAbbreviation:")
+    print(dfGads[["StateName", "StateAbbreviation"]].head(10))
+
+    # Filter dfGads based on the StateAbbreviation being in veloStates
+    dfGadsFilt = dfGads[dfGads["StateAbbreviation"].isin(veloStates)]
+
+    # Drop the temporary 'StateAbbreviation' column
+    dfGadsFilt = dfGadsFilt.drop(columns=["StateAbbreviation"])
+    
+    return dfGadsFilt
+
 
 # %%
